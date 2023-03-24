61508c26f53463a682b8d59ac65092db036d138b
Pratik Sanyaja <pratiksanyaja1998@gmail.com>
2023-03-24 16:42:55 +0530

    Fix build

--- example/src/index.js
+++ example/src/index.js
@@ -1,6 +1,13 @@
 
 import React from 'react'
-import ReactDOM from 'react-dom'
 import App from './App'
+import * as ReactDOMClient from 'react-dom/client';
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const container = document.getElementById('root');
+
+// Create a root.
+const root = ReactDOMClient.createRoot(container);
+
+// Initial render: Render an element to the root.
+root.render(<App  />);
+

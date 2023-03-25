7a36f9acd94ff352761dac01873f1695c8493479
Hardik Nariya <hardiknariya308@gmail.com>
2023-03-25 11:59:54 +0530

    fix bugs

--- example/src/index.js
+++ example/src/index.js
@@ -1,12 +1,11 @@
-import React from 'react';
-import App from './App';
-import * as ReactDOMClient from 'react-dom/client';
+import React from 'react'
+import App from './App'
+import * as ReactDOMClient from 'react-dom/client'
 
-const container = document.getElementById('root');
+const container = document.getElementById('root')
 
 // Create a root.
-const root = ReactDOMClient.createRoot(container);
+const root = ReactDOMClient.createRoot(container)
 
 // Initial render: Render an element to the root.
-root.render(<App />);
-
+root.render(<App />)

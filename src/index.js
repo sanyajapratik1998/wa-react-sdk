3c3da7d0ecd3296d6d3d260ad8112e6edbfd45ed
Hardik Nariya <hardiknariya308@gmail.com>
2023-03-25 11:03:41 +0530

    remove extra line and update version

--- src/index.js
+++ src/index.js
@@ -10,7 +10,6 @@ import {
   REHYDRATE,
 } from "redux-persist";
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import storage from "redux-persist/lib/storage";
 import { combineReducers } from "redux";
 import { configureStore } from "@reduxjs/toolkit";
 import { PersistGate } from "redux-persist/integration/react";
